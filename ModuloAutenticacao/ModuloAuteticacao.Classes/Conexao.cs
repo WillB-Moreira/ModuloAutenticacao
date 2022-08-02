44410d9d443bc6a5069d05cbb6ee36a013bdc6d3
WillB-Moreira <wmup73@gmail.com>
2022-08-02 11:45:10 -0300

    Adicionando Clone Projeto

---
+++ ModuloAutenticacao/ModuloAuteticacao.Classes/Conexao.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace ModuloAuteticacao.Classes
+{
+    internal class Conexao
+    {
+
+        public static SqlConnection _conn;
+
+        public static SqlConnection MinhaInstancia
+        { //a chave da propriedade
+            get
+            { // chave do método get
+              //se não existe conexão.
+                if (_conn == null)
+                { // chave do if
+                    //Criar a conexão com SqlServer // Nos Parentes - Connection String com os dados do SQLSERVER 
+                    _conn = new SqlConnection(@"Server = Lab206_2; Database = ProjetoEstoquev; Uid = sa; Pwd = texte*123;");
+                } // fecha chave do if
+                  //retorna a conexão
+                return _conn;
+            }
+        }
+    }
+}
+
+
+
+    
---
+++ ModuloAutenticacao/ModuloAuteticacao.Desktop/TelaNivel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModuloAuteticacao.Classes;
+
+namespace ModuloAuteticacao.Desktop
+{
+    public partial class TelaNivel : Form
+    {
+        public TelaNivel()
+        {
+            InitializeComponent();
+        }
+
+        private void TelaNivel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblId_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            NivelDAO nivel = new NivelDAO();
+            MessageBox.Show(nivel.Inserir(txtNome.Text));
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            NivelDAO nivel = new NivelDAO();
+            MessageBox.Show(nivel.Atualizar());
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            NivelDAO nivel = new NivelDAO();
+            MessageBox.Show(nivel.Pesquisar());
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            NivelDAO nivel = new NivelDAO();
+            MessageBox.Show(nivel.Deletar());
+        }
+    }
+}
